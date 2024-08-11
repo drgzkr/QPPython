e0b659b6e38d2fbfa6e74fc55d4ea633d606766c
drgzkr <60198204+drgzkr@users.noreply.github.com>
2024-08-11 05:41:13 +0200

    Create simulated_data_example.py

---
+++ examples/simulated_data_example.py
@@ -0,0 +1,51 @@
+# Simulates random data and visulizes results
+import numpy as np
+from QPPython import find_qpp
+'''
+Create simulated data
+'''
+npt = 10 # number of template timepoints, template window size
+nX = 120 # number of voxels
+nT = 450 # number of total timepoints
+#ns =  nT - npt # Number of extractable windows in the data
+
+data = np.random.rand(nX, nT)
+
+# initial starting template from the data
+random_starting_point = np.random.randint(0,nT-npt)
+print('Started with initial qpp at: ',random_starting_point)
+T = data[:,random_starting_point:random_starting_point+npt]
+
+qpp_pattern, cn = find_qpp(data,T)
+
+peaks, _ = find_peaks(cn)
+
+'''
+Plot the qpp similarity/convolution timeseries
+'''
+fig, axs = plt.subplots(figsize=(12,4))
+fig.suptitle('QPP Timeseries and Peaks')
+axs.plot(cn)
+for peak in peaks:
+  axs.axvline(x=peak,alpha=.5,c='orange')
+  
+axs.axvline(x=random_starting_point,alpha=1,c='red',label='Initial QPP')
+axs.axvline(x=random_starting_point+npt,alpha=1,c='red')
+axs.legend()
+
+'''
+Plot the qpp pattern
+'''
+fig, axs = plt.subplots(1,3,figsize=(12,5),width_ratios=(3,1,1))
+
+axs[0].set_title('Data')
+axs[0].imshow(data,aspect='auto')
+axs[0].axvline(x=random_starting_point,c='red',label='First QPP')
+axs[0].axvline(x=random_starting_point+npt,c='red')
+axs[0].legend()
+
+axs[1].set_title('First QPP')
+axs[1].imshow(T)
+
+axs[2].set_title('Final QPP')
+axs[2].imshow(qpp_pattern)
